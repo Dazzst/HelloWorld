eb4a70a88078ef4f58b06ec1790e2744f5e2bf4e
Dazzst <lstlst200@gmail.com>
2020-09-08 18:47:54 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Pause to display Hello World!
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true); 
+           
+        }
+    }
+}
